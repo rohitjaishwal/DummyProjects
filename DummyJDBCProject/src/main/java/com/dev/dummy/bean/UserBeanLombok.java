ed487133a7ee96633bbd607f3972b259207a5215
Rohit <Rohit@192.168.1.36>
2019-10-18 15:20:44 +0530

    First Commit

---
+++ DummyJDBCProject/src/main/java/com/dev/dummy/bean/UserBeanLombok.java
@@ -0,0 +1,26 @@
+package com.dev.dummy.bean;
+
+import java.io.Serializable;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@SuppressWarnings("serial")
+@AllArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode
+@ToString
+public class UserBeanLombok implements Serializable {
+	
+	public UserBeanLombok() {
+		super();
+	}
+	private int userid;
+	private String username;
+	private String email;
+	@ToString.Exclude private String password;
+}
